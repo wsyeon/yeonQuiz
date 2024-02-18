5588f150eef1fb357e3d8152652fd0ccf53a1a4a
C팀 <sywon2005@gmail.com>
2024-02-19 04:49:20 +0900

    1시간마다 풀 수 있게 설정

--- src/pages/ResultPage.js
+++ src/pages/ResultPage.js
@@ -4,23 +4,23 @@ import { useLocation, useNavigate } from "react-router-dom";
 const ResultPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  // const [result, setResult] = useState(true);
+  const { name, total } = location.state;
 
-  // useEffect(() => {
-  //   if (location.state.total < 15) {
-  //     setResult((pre) => !pre);
-  //   }
-  // }, [location.state.total]);
+  useEffect(() => {
+    const userInfo = JSON.stringify({
+      user: name,
+      time: new Date(),
+      record: "taken",
+    });
+    localStorage.setItem("quizStatus", userInfo);
+  }, [name]);
 
   return (
     <div>
       <div>끝</div>
-      <div>{location.state.total}</div>
-      <button
-        onClick={() => navigate("/", { state: { name: location.state.name } })}
-      >
-        다시 하기
-      </button>
+      <div>{name}</div>
+      <div>{total}</div>
+      <button onClick={() => navigate("/")}>홈으로</button>
     </div>
   );
 };
--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,9 +1,41 @@
-import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const StartPage = () => {
-  const location = useLocation();
-  const [nickName, setNickName] = useState(location.state?.name || "");
+  const { record, time, user } =
+    JSON.parse(localStorage.getItem("quizStatus")) ?? "";
+  const [taken, setTaken] = useState(false);
+  const [name, setName] = useState(user ?? "");
+  const navigate = useNavigate();
+
+  const now = new Date();
+  const lastQuiz = new Date(time);
+  const timeDiff = now - lastQuiz;
+
+  useEffect(() => {
+    if (record === "taken" && time) {
+      const oneHour = 60 * 60 * 1000;
+      if (timeDiff >= oneHour) {
+        localStorage.removeItem("quizStatus");
+        setTaken(false);
+      } else {
+        setTaken(true);
+      }
+    }
+  }, [record, time, timeDiff]);
+
+  const onClick = (taken) => {
+    const timer = 3600 - Math.trunc(timeDiff / 1000);
+    if (taken) {
+      if (timer < 0) {
+        alert("새로고침을 해주세요");
+      } else {
+        alert(`${timer}초 남았습니다`);
+      }
+    } else {
+      navigate(`/step?name=${name}`);
+    }
+  };
 
   return (
     <div>
@@ -11,16 +43,15 @@ const StartPage = () => {
         <input
           type="text"
           placeholder="이름"
-          value={nickName}
-          disabled={location.state?.name ? true : false}
+          value={name}
           onChange={(e) => {
             e.preventDefault();
-            setNickName(e.target.value);
+            setName(e.target.value);
           }}
         />
-        <Link to={`/step?name=${nickName}`}>
-          <button>시작하기</button>
-        </Link>
+        <button disabled={name.trim() === ""} onClick={() => onClick(taken)}>
+          시작하기
+        </button>
       </div>
     </div>
   );
