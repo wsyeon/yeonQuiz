84aa0aa55ef0573269560c80243044df8023c9ae
C팀 <sywon2005@gmail.com>
2024-02-24 03:18:08 +0900

    시간제한 기능 구현

--- src/pages/StepPage.js
+++ src/pages/StepPage.js
@@ -1,19 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import data from "../common/api/question.json";
 import { Div, Footer, Header, Wrapper, BorderInput } from "./StartPage";
+import { MdTimer } from "react-icons/md";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoSearch } from "react-icons/io5";
 import { AiOutlineMenu } from "react-icons/ai";
 import { CiSquarePlus } from "react-icons/ci";
-import { styled } from "styled-components";
+import styled from "styled-components";
 
 const StepPage = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const [step, setStep] = useState(searchParams.get("step") ?? 0);
   const [point, setPoint] = useState(searchParams.get("point") ?? 0);
+  const [second, setSecond] = useState(10);
   const name = searchParams.get("name");
+  const count = useRef(10);
+  const interval = useRef(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      count.current -= 0.1;
+      setSecond((prev) => (prev -= 0.1));
+    }, 105);
+  }, [step]);
+
+  useEffect(() => {
+    if (count.current <= 0) {
+      count.current = 10;
+      setSecond(10);
+      setStep((prev) => prev + 1);
+      clearInterval(interval.current);
+      setPoint((prev) => prev + 0);
+      searchParams.set("step", step + 1);
+      searchParams.set("point", point + 0);
+      setSearchParams(searchParams);
+    }
+  }, [second, step, point, searchParams, setSearchParams]);
 
   useEffect(() => {
     if (step > 9) {
@@ -27,11 +51,14 @@ const StepPage = () => {
   }, [navigate, point, name, step]);
 
   const goNextStep = (num) => {
+    count.current = 10;
+    setSecond(count.current);
     setStep((prev) => prev + 1);
     setPoint((prev) => prev + parseInt(data[step].answers[num].point));
     searchParams.set("step", step + 1);
     searchParams.set("point", point + parseInt(data[step].answers[num].point));
     setSearchParams(searchParams);
+    clearInterval(interval.current);
   };
 
   return (
@@ -45,7 +72,18 @@ const StepPage = () => {
           </div>
         </Header>
         <Body>
-          <div style={{ width: 40, margin: "0 auto" }}>
+          <PrograssBarWrap>
+            <MdTimer size={32} />
+            <PrograssBar $time={second} />
+          </PrograssBarWrap>
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <h1>Q{step + 1}</h1>
           </div>
 
@@ -72,7 +110,7 @@ export default StepPage;
 
 const Body = styled.div`
   width: 350px;
-  height: 330px;
+  height: auto;
   background-color: white;
   border-radius: 20px;
   h1 {
@@ -84,7 +122,7 @@ const Body = styled.div`
 `;
 
 const Question = styled.p`
-  font-size: 15px;
+  font-size: 18px;
   line-height: 22px;
   margin: 28px auto;
   text-align: center;
@@ -115,4 +153,21 @@ export const Button = styled.button`
   &:last-child {
     margin-bottom: 20px;
   }
+`;
+
+const PrograssBar = styled.div`
+  background-color: #fee500;
+  width: ${(props) => props.$time * 10}%;
+  height: 50%;
+  transition: 0.3;
+  border-radius: 20px;
+  border: none;
+`;
+const PrograssBarWrap = styled.div`
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  box-sizing: border-box;
 `;
